853edb95ce7002b19cb8dc21243e375634cf0906
Stevie Merken <stevie.merken@gmail.com>
2025-06-01 19:02:29 +0200

    section 4

---
+++ Section4/EntityFrameworkCore.Console/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using EntityFrameworkCore.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var context = new FootballLeagueDBContext();
+
+        //await GetAllTeams(context);
+
+        //var team = await context.Teams.FindAsync(5);
+
+        //var avg = await context.Teams.AverageAsync(t => t.TeamId);
+
+        //var groupedTeams = context.Teams.GroupBy(t => t.DateCreated.Date)
+        //    .Select(g => new
+        //    {
+        //        DateCreated = g.Key,
+        //        NumerOfTeams = g.Count()
+        //    })
+        //    .Where(g => g.NumerOfTeams > 1)
+        //    .ToList();
+
+        //var teams = await context.Teams
+        //    .Select(t => new {
+        //        t.Name,
+        //        t.DateCreated,
+        //    })
+        //    .ToListAsync();
+
+        //foreach (var team in teams)
+        //{
+        //    Console.WriteLine($"{team}");
+        //}
+
+        var teams = await context.Teams
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    private static async Task GetAllTeams(FootballLeagueDBContext context)
+    {
+        var teams = await context.Teams.ToListAsync();
+
+        foreach (var team in teams)
+        {
+            Console.WriteLine($"Team ID: {team.TeamId}, Name: {team.Name}, Date Created: {team.DateCreated}");
+        }
+    }
+}
---
+++ Section4/EntityFrameworkCore.Data/FootballLeagueDBContext.cs
@@ -0,0 +1,37 @@
+﻿using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Data;
+
+public class FootballLeagueDBContext : DbContext
+{
+    public FootballLeagueDBContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Path.Combine(Environment.GetFolderPath(folder), "EntityFrameworkCore");
+        Directory.CreateDirectory(path);
+        DbPath = Path.Combine(path, "FootballLeague.db");
+    }
+
+    public string DbPath { get; set; }
+
+    public DbSet<Team> Teams { get; set; }
+
+    public DbSet<Coach> Coaches { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={DbPath}")
+            .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Team>().HasData(
+            new Team { TeamId = 1, Name = "Team A", DateCreated = new DateTime(2025, 5, 29, 13, 34, 26, 346, DateTimeKind.Unspecified).AddTicks(2056) },
+            new Team { TeamId = 2, Name = "Team B", DateCreated = new DateTime(2025, 5, 29, 13, 34, 26, 346, DateTimeKind.Unspecified).AddTicks(2056) }
+        );
+    }
+}
